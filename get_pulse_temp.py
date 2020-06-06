22b784101e5dddbe8ca813489f7b509f77c4a0eb
yphsieh <melodyhsieh44@gmail.com>
2020-06-06 17:46:10 +0800

    --

--- get_pulse_temp.py
+++ get_pulse_temp.py
@@ -359,16 +359,18 @@ def get_pulse(args):
 
     with open('recordings.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerow(['Time', 'BPM', 'TEMP', 'Nothing'])
+        writer.writerow(['','Time', 'BPM', 'TEMP', 'Nothing'])
 
     delay = 0
     while App.kill == False:
+        index = 0 
         App.main_loop()
         if delay % 5 == 0: App.Tx = True
         if len(App.processor.ttimes) and len(App.processor.ttimes) % 3 == 0:
             with open('recordings.csv', 'a', newline='') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow([App.processor.ttimes[-1], App.processor.bpms[-1], App.processor.temps[-1], 0])
+                writer.writerow([index, App.processor.ttimes[-1], App.processor.bpms[-1], App.processor.temps[-1], 0])
+                index += 1
         delay += 1
 
 
