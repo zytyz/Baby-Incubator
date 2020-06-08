255a6337268407379f60c8b772fa54f4625d9248
子宜 <polarbear0512@gmail.com>
2020-06-08 16:28:16 +0800

    add conda env as variable

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-# bash run.sh <subject_name> <webcam url> <http ip>
-# example: bash run.sh zyt http://192.168.0.108:8080/video 192.168.0.102
+# bash run.sh <subject_name> <webcam url> <http ip> <conda env name>
+# example: bash run.sh zyt http://192.168.0.108:8080/video 192.168.0.102 py37
 SUBJECT_NAME=$1
 WEBCAM_URL=$2
 HTTP_IP=$3
-tmux new-session -s incubator -d "conda activate py37; cd ~/Desktop/Baby-Incubator; python get_pulse_temp.py --subject $SUBJECT_NAME --url $WEBCAM_URL"
-tmux new-window -t incubator:1 "conda activate py37; cd ~/Desktop/Baby-Incubator; python connect_app.py $HTTP_IP"
+CONDA_ENV_NAME=$4
+tmux new-session -s incubator -d "source deactivate; conda activate $CONDA_ENV_NAME; cd ~/Desktop/Baby-Incubator; python get_pulse_temp.py --subject $SUBJECT_NAME --url $WEBCAM_URL"
+tmux new-window -t incubator:1 "source deactivate; conda activate $CONDA_ENV_NAME; cd ~/Desktop/Baby-Incubator; python connect_app.py $HTTP_IP"
 tmux attach -t incubator
